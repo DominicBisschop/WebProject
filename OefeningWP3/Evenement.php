91e549ccf14c0ede66e351aa52dcc9d51924cb23
11400523 <11400523@student.pxl.be>
2017-05-03 10:10:40 +0200

    WP3

---
+++ OefeningWP3/Evenement.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+    </head>
+
+    <body>
+    <img src="monkey_logo.jpg" width="100px">
+    <h1>Monkey Bussiness</h1>
+    <h2>Evenementen</h2>
+    <button type="button">Aanmaken</button>
+    <button type="button">Wijzigen</button>
+    <button type="button">Verwijderen</button>
+        <table>
+            <tr>
+                <th>   </th>
+                <th>Naam</th>
+                <th>Datum</th>
+            </tr>
+            <tr>
+                <th><input type="checkbox "name=selected"></th>
+                <th>Henkens</th>
+                <th>31 februarie</th>
+            </tr>
+            <tr>
+                <th><input type="checkbox "name=selected">"</th>
+                <th>evenement2</th>
+                <th>5 april</th>
+            </tr>
+        </table>
+    </body>
+</html>
