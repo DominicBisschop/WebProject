98c4adfd2e53d71c5c697d676648c2d1b8c0e4b4
11400523 <11400523@student.pxl.be>
2017-05-23 22:55:45 +0200

    .

--- OefeningWP3/Evenement.php
+++ OefeningWP3/Evenement.php
@@ -29,7 +29,6 @@
     var container = document.getElementById("table");
     var getID = document.getElementById("getID").value;
     var delID = document.getElementById("delID").value;
-    var url = "";//todo
 
     document.getElementById("searchButton").addEventListener("click",myFunction);
     function myFunction(){
